e958fcdc1d86f81b68551661e751c29035921790
avaashh <avaashbhattarai@gmail.com>
2026-02-23 11:33:38 -0600

    Message type

--- headers/message.h
+++ headers/message.h
@@ -2,7 +2,10 @@
 
 #include "headers/config.h"
 
-#define MAX_MESSAGE_LENGTH 2048
+typedef struct {
+    enum message_type type;
+    char body[MAX_MESSAGE_LENGTH];
+} Message;
 
 // Send a across a socket with a header that includes the message length. Returns non-zero value if
 // an error occurs.
@@ -10,4 +13,4 @@ int send_message(int fd, enum message_type type, char* message);
 
 // Receive a message from a socket and return the message string (which must be freed later).
 // Returns NULL when an error occurs.
-char* receive_message(int fd);
+Message* receive_message(int fd);
--- message.c
+++ message.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "headers/config.h"
+#include "headers/message.h"
 
 char* MESSAGE_PREFIXES_ARR[] = MESSAGE_PREFIXES;
 
@@ -50,8 +50,8 @@ int send_message(int fd, enum message_type type, char* message) {
   return 0;
 }
 
-// Receive a message from a socket and return the message string (which must be freed later)
-char* receive_message(int fd) {
+// Receive a message from a socket and return the message (which must be freed later)
+Message* receive_message(int fd) {
   // First try to read in the message length
   size_t len;
   if (read(fd, &len, sizeof(size_t)) != sizeof(size_t)) {
@@ -66,13 +66,47 @@ char* receive_message(int fd) {
   }
 
   // Allocate space for the message and a null terminator
-  char* result = malloc(len + 1);
+  Message* result = malloc(sizeof(Message));
 
-  // Try to read the message. Loop until the entire message has been read.
+  // read the first MESSAGE_PREFIX_LENGTH chars to determine message type, then read the rest of the message body
+  char prefix[MESSAGE_PREFIX_LENGTH + 1];
+  ssize_t rc = read(fd, prefix, MESSAGE_PREFIX_LENGTH);
+  if (rc != MESSAGE_PREFIX_LENGTH) {
+    free(result);
+    return NULL;
+  }
+  prefix[MESSAGE_PREFIX_LENGTH] = '\0';
+
+  // Find the message type based on the prefix
+  result->type = -1;
+  for (int i = 0; i < 5; i++) {
+    if (strcmp(prefix, MESSAGE_PREFIXES_ARR[i]) == 0) {
+      result->type = i;
+      break;
+    }
+  }
+
+  if (result->type == -1) {
+    // could not find a valid message type
+    free(result);
+    errno = EINVAL;
+    return NULL;
+  }
+
+  // len is now the actual body length
+  len -= MESSAGE_PREFIX_LENGTH + 1; 
   size_t bytes_read = 0;
+
+  // discard ":"
+  char discard;
+  if (read(fd, &discard, 1) != 1) {
+    free(result);
+    return NULL;
+  }
+
   while (bytes_read < len) {
     // Try to read the entire remaining message
-    ssize_t rc = read(fd, result + bytes_read, len - bytes_read);
+    ssize_t rc = read(fd, result->body + bytes_read, len - bytes_read);
 
     // Did the read fail? If so, return an error
     if (rc <= 0) {
@@ -85,7 +119,6 @@ char* receive_message(int fd) {
   }
 
   // Add a null terminator to the message
-  result[len] = '\0';
-
+  result->body[len] = '\0';
   return result;
 }
