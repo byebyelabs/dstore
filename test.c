eb8bc6fb1516fb0f8bc021fb25b92ae459f5f92d
avaashh <avaashbhattarai@gmail.com>
2026-02-23 22:36:57 -0600

    simple test

--- test.c
+++ test.c
@@ -1,28 +1,20 @@
-#include "headers/storage.h"
-#include <assert.h>
-#include <string.h>
+#include "client.h"
+#include "config.h"
+
 #include <stdio.h>
 
 int main() {
-  set("key", "value");
-  set("key2", "another value");
-  set("key", "not the original value");
-  set("key3", "even more values?");
-  set("key4", "does it keep going down?");
-  set("key5", "i think it does");
+  dset("hello", "julian");
+  printf("set {hello: julian}\n");
 
-  printf("testing key2\n");
-  const char* key2_value = get("key2");
-  assert(key2_value != NULL && strcmp(key2_value, "another value") == 0);
-  printf("testing key again\n");
-  const char* key_value = get("key");
-  assert(key_value != NULL && strcmp(key_value, "not the original value") == 0);
-  printf("done\n");
+  char value1[MAX_VALUE_LENGTH];
+  dget("hello", value1);
+  printf("got value: %s\n", value1);
 
-  print_list();
-  printf("deleting key\n");
-  assert(del("key") == 0);
-  print_list();
-  
-  return 0;
+  dset("hello", "devanshu");
+  printf("set {hello: devanshu}\n");
+
+  char value2[MAX_VALUE_LENGTH];
+  dget("hello", value2);
+  printf("got value: %s\n", value2);
 }
