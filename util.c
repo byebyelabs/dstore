601a2dc42ffd6930df79f4627bd4bfd48924c211
avaashh <avaashbhattarai@gmail.com>
2026-02-24 22:34:55 -0600

    hashing is too hard bro

--- util.c
+++ util.c
@@ -3,7 +3,12 @@
 #include <string.h>
 
 void hash_string(const char *str, unsigned char *hash_out) {
-  EVP_Q_digest(NULL, "MD5", NULL, str, strlen(str), hash_out, NULL);
+  // Getting rid of MD5 hashing because it is too buggy for 
+  // our use case. our hash = truncate key at 16 chars and
+  // pad with ' ' to get 16 byte hash.
+  memset(hash_out, ' ', HASH_LENGTH);
+  int len = strlen(str);
+  strncpy((char *)hash_out, str, len > HASH_LENGTH ? HASH_LENGTH : len);
 }
 
 int hash_cmp(const unsigned char *a, const unsigned char *b) {
