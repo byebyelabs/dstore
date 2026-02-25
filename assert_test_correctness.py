0a331d6b26d7cd665513a207e45a4e51fca0b19d
avaashh <avaashbhattarai@gmail.com>
2026-02-24 23:11:02 -0600

    test correctness ensurer

---
+++ assert_test_correctness.py
@@ -0,0 +1,37 @@
+MAP = {}
+
+# ensure all log entries are correct
+with open("test_log.txt", "r") as test_logs:
+    for i, test in enumerate(test_logs):
+        ts, tst = test.split(" | ")
+        typ, response = tst.split(": ")
+        key, val = response.split(" -> ")
+
+        key = key.strip()
+        val = val.strip()
+
+        if typ == "SET":
+            MAP[key] = val
+
+        elif typ == "GET":
+            should_be = MAP.get(key, "")
+            assert (
+                should_be == val
+            ), f"line {i}: GET {key} should be {should_be} but got {val}"
+
+        else:
+            assert False, "INVALID LOG ENTRY"
+
+print("all SETs and GETs are consistent")
+
+# print summary stats
+for op in ["set", "get"]:
+    with open(f"{op}_log.txt", "r") as logs:
+        ttime = 0
+        count = 0
+        for line in logs:
+            start, end = line.split()
+            ttime += int(end) - int(start)
+            count += 1
+
+    print(f"avg latency for {op}: {ttime / count}ms over {count} operations")
